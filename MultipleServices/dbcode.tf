d05304fd951ab4fc4ee6c2c8c4b8ca2e209fc08a
Rod Stewart <rodstewart@gmail.com>
2019-12-20 00:23:25 -0600

    Corrects multiple issues with Multi-Service demo

--- MultipleServices/dbcode.tf
+++ MultipleServices/dbcode.tf
@@ -1,8 +1,8 @@
 # SQL SERVER
 resource "azurerm_sql_server" "sqlserver" {
   name                         = "avasql1"
-  resource_group_name          = "${azurerm_resource_group.resource_gp.name}"
-  location                     = "${azurerm_resource_group.resource_gp.location}"
+  resource_group_name          = azurerm_resource_group.multi_deploy_rg.name
+  location                     = azurerm_resource_group.multi_deploy_rg.location
   version                      = "12.0"
   administrator_login          = "rnadmin"
   administrator_login_password = "4p455w0rd"
@@ -10,16 +10,16 @@ resource "azurerm_sql_server" "sqlserver" {
 
 resource "azurerm_sql_virtual_network_rule" "sqlvnetrule" {
   name                = "sql-vnet-rule"
-  resource_group_name = "${azurerm_resource_group.resource_gp.name}"
-  server_name         = "${azurerm_sql_server.sqlserver.name}"
-  subnet_id           = "${azurerm_subnet.dbsub.id}"
+  resource_group_name = azurerm_resource_group.multi_deploy_rg.name
+  server_name         = azurerm_sql_server.sqlserver.name
+  subnet_id           = azurerm_subnet.dbsub.id
 }
 
 # DB SUBNET
 resource "azurerm_subnet" "dbsub" {
   name                 = "datasub"
-  resource_group_name  = "${azurerm_resource_group.resource_gp.name}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  resource_group_name  = azurerm_resource_group.multi_deploy_rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefix       = "10.0.3.0/24"
   service_endpoints    = ["Microsoft.Sql"]
 }
--- MultipleServices/main.tf
+++ MultipleServices/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "multi_deploy_rg" {
   name     = "multiservicedeploy"
   location = "eastus"
 
-  tags {
+  tags = {
     Owner = "Rod Stewart"
   }
 }
@@ -14,33 +14,33 @@ variable "prefix" {
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.multi_deploy_rg.location}"
-  resource_group_name = "${azurerm_resource_group.multi_deploy_rg.name}"
+  location            = azurerm_resource_group.multi_deploy_rg.location
+  resource_group_name = azurerm_resource_group.multi_deploy_rg.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = "${azurerm_resource_group.multi_deploy_rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  resource_group_name  = azurerm_resource_group.multi_deploy_rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = "${azurerm_resource_group.multi_deploy_rg.location}"
-  resource_group_name = "${azurerm_resource_group.multi_deploy_rg.name}"
+  location            = azurerm_resource_group.multi_deploy_rg.location
+  resource_group_name = azurerm_resource_group.multi_deploy_rg.name
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
+    subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "dynamic"
   }
 }
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = "${azurerm_resource_group.multi_deploy_rg.location}"
-  resource_group_name   = "${azurerm_resource_group.multi_deploy_rg.name}"
+  location              = azurerm_resource_group.multi_deploy_rg.location
+  resource_group_name   = azurerm_resource_group.multi_deploy_rg.name
   network_interface_ids = ["${azurerm_network_interface.main.id}"]
   vm_size               = "Standard_DS1_v2"
 
@@ -74,7 +74,7 @@ resource "azurerm_virtual_machine" "main" {
     disable_password_authentication = false
   }
 
-  tags {
+  tags = {
     Owner = "Rod Stewart"
   }
 }
